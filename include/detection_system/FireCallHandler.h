af34a8e5bdd9726bee07933f0b3c91ab667720aa
erenalptunay <tunayerenalp@gmail.com>
2025-12-14 23:38:51 +0300

    detection: refactor FireCallHandler to separate source and header

--- include/detection_system/FireCallHandler.h
+++ include/detection_system/FireCallHandler.h
@@ -6,10 +6,7 @@
 // Handles the final emergency response (external call).
 class FireCallHandler : public EmergencyHandler {
 public:
-    void handleRequest() override {
-        cout << "[System] Calling Fire Station..." << endl;
-        cout << "[System] Connection established. Emergency reported." << endl;
-    }
+    void handleRequest() override;
 };
 
 #endif
