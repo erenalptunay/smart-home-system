b4cbdf7e66d1d35d6db941a5a21af512f193650d
Berkay Demirci <143441795+BerkaayD@users.noreply.github.com>
2025-12-14 18:23:05 +0300

    Refactor Alarm class to include getFullType method
    Added getFullType method to return the type of Alarm.

--- include/device_hierarchy/Alarm.h
+++ include/device_hierarchy/Alarm.h
@@ -2,48 +2,51 @@
 #define ALARM_H
 
 #include "Device.h"
-#include <iostream>
-
-using namespace std;
 
 class Alarm : public Device {
 private:
-    static Alarm* instance;   // Singleton instance
+	static Alarm* instance;   // Singleton instance
 
-    // Private constructor (Singleton)
-    Alarm(string n) : Device(n) {
-        cout << "Alarm Sistemi (" << n << ") baslatildi." << endl;
-    }
+
+	// Private constructor (Singleton)
+	Alarm(string n) : Device(n) {
+		cout << "Alarm Sistemi (" << n << ") baslatildi." << endl;
+	}
 
 public:
-    // Singleton eriþim fonksiyonu
-    static Alarm* getInstance(string n) {
-        if (instance == nullptr) {
-            instance = new Alarm(n);
-        }
-        return instance;
-    }
+	// Singleton eriÅŸim fonksiyonu
+	static Alarm* getInstance(string n) {
 
-    // Device'tan override edilen close
-    void close() override {
-        cout << "[ALARM] Alarm sistemi kapatiliyor..." << endl;
-        Device::close();
-    }
+		if (instance == nullptr) {
+			instance = new Alarm(n);
+		}
+		return instance;
+	}
 
-    // Prototype (Singleton olduðu için kopya yok)
-    Device* clone() const override {
-        return nullptr;
-    }
 
-    // Alarm'a özel fonksiyon
-    void activate() {
-        if (openCheck) {
-            cout << "Alarm aktif edildi ve izleniyor." << endl;
-        }
-        else {
-            cout << "Once cihazi acmaniz gerekir." << endl;
-        }
-    }
+	void close() override {
+		cout << "[ALARM] Alarm sistemi kapatiliyor..." << endl;
+		Device::close();
+	}
+
+	// Prototype (Singleton olduÄŸu iÃ§in kopya yok)
+	Device* clone() const override {
+		return nullptr;
+	}
+
+	string getFullType() const override {
+		return "Alarm";
+	}
+
+	// Alarm'a Ã¶zel fonksiyon
+	void activate() {
+		if (openCheck) {
+			cout << getFullType() << " aktif edildi ve izleniyor." << endl;
+		}
+		else {
+			cout << getFullType() << " acik degil. Once cihazi acmaniz gerekir." << endl;
+		}
+	}
 };
 
 /* -------- static member definition -------- */
