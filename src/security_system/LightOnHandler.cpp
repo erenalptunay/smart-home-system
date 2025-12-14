39dfa7777a299a92c61d579e8a2104bb7948956c
Funlu24 <funlu8748@gmail.com>
2025-12-15 01:32:35 +0300

    .

--- src/security_system/LightOnHandler.cpp
+++ src/security_system/LightOnHandler.cpp
@@ -14,10 +14,11 @@ void LightOnHandler::handleRequest(SecurityEvent event) {
 		std::this_thread::sleep_for(std::chrono::seconds(3));  //3 saniye bekle
 
 		for (Device* d : allDevices_) {  // Tüm cihazlarý tek tek dolaþ 
-		
+
 			if (Light* l = dynamic_cast<Light*>(d)) { // Cihaz bir Light (Lamba) ise ozaman artýk onu acýp kapatabiliriz.
-				
+
 				l->connect();  // Lambayý açma iþlemi için connect metodunu çaðýr oraya bakar arýza olup olmadýðýný kontrol eder.açýk olup olmadýðýný kontrol et acýk deðilse açar.
+
 			}
 		}
 
