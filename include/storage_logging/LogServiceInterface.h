ca01377417f65408a61bd676e8031cdbcb34cb4d
Berkan <berkancylnn@gmail.com>
2025-12-14 15:54:11 +0300

    Storage:: Add comment lines and console outputs.

--- include/storage_logging/LogServiceInterface.h
+++ include/storage_logging/LogServiceInterface.h
@@ -8,7 +8,7 @@ class LogServiceInterface
 public:
 	virtual ~LogServiceInterface() = 0;
 	virtual bool Start() = 0;
-	virtual void writeLog(const std::string& message,std::string source) = 0;
+	virtual void writeLog(const std::string& message, std::string source) = 0;
 	virtual void Close() = 0;
 };
 
