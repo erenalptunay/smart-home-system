30a49f20a3ef15148eef044d28021463b79dafd9
erenalptunay <tunayerenalp@gmail.com>
2025-12-14 23:38:51 +0300

    detection: refactor FireCallHandler to separate source and header

---
+++ src/detection_system/FireCallHandler.cpp
@@ -0,0 +1,9 @@
+#include "../../include/detection_system/FireCallHandler.h"
+#include <iostream>
+
+using namespace std;
+
+void FireCallHandler::handleRequest() {
+    cout << "[System] Calling Fire Station..." << endl;
+    cout << "[System] Connection established. Emergency reported." << endl;
+}
